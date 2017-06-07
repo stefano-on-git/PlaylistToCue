8888a5ecf1936d9310cdfe7a0943cd4aad778855
Stefano - Ufficio <stefano.romani@vilcor.it>
2017-06-07 11:29:11 +0200

    Aggornato il percorso della dir publish

--- PlaylistToCue.bat
+++ PlaylistToCue.bat
@@ -2,7 +2,8 @@
 
 REM title PlaylistToCue
 
-cd C:\Users\stefano\Documents\Programmazione\Progetti\DotNetCore\PlaylistToCue\bin\Debug\netcoreapp1.1\win7-x64\publish
+cd .\bin\BPC\Debug\netcoreapp1.1\win81-x64\publish
+REM cd .\bin\BPC\Debug\netcoreapp1.1\win7-x64\publish
 
 PlaylistToCue.exe
 
